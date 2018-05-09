6a5c7178e28fd1c9f42f2ee83da036c05ed2164c
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-05-09 15:53:29 -0700

    Update WebsiteTraffic.py

--- WebsiteTraffic.py
+++ WebsiteTraffic.py
@@ -1,3 +1,38 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd
+from selenium import webdriver
+from urllib.request import urlopen
+import time
+
+class GoogleAnalyticsTrafficMonitor(object):
+
+
+
+    #Set Browser, add ability to switch/detect to other browser types
+    #browser = webdriver.Firefox()
+    browser = webdriver.Chrome()
+
+
+    #Log In to Google Analytics
+    login_address = input()   
+
+    #Get
+    browser.get(login_address)
+
+    #Wait for response with sleep
+    time.sleep(10)
+    
+    #Get Username and Password to log in
+    username = input("Username")
+    password = input("Password")
+
+    #Locate login locations
+    browser.find_element_by_id("")
+    browser.find_element_by_id("")
+    
+    #Apply username/pass keys
+    username.send_keys(username)
+    password.send_keys(password)
+    
+    #Attempt Login
+    login = browser.find_element_by_xpath("//*[@type='submit']")
+    login.submit()
